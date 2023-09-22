2c167af116ffc6688b4f741ea8297cd1b556a3aa
ZYPZ <72951371+ZYPZ33@users.noreply.github.com>
2023-09-22 11:25:08 +0100

    Half-Works

--- tor-router-qt.py
+++ tor-router-qt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from sys import argv, exit
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
 from PyQt5.QtGui import QPixmap
@@ -11,15 +12,21 @@ class MainWindow(QWidget):
 
         self.resize(300, 100)
 
+        # Enable Tor-Router button
         self.button = QPushButton("Enable Tor-Router", self)
         self.button.setGeometry(80, 10, 160, 30)
         self.button.clicked.connect(on_button_click)
 
+        # Image
         self.image_label = QLabel(self)
         self.image_label.setGeometry(10, 10, 75, 75)
         self.image_label.setPixmap(QPixmap("tor-router.svg"))
 
-        self.text_label = QLabel("Tor-Router is disabled", self)
+        # "Tor-Router is disabled" Text
+        if system("test `systemctl is-active tor-router` = 'enabled'"):
+            self.text_label = QLabel("Tor-Router is disabled", self)
+        else:
+            self.text_label = QLabel("Tor-Router is enabled", self)
         self.text_label.setGeometry(85, 50, 150, 20)
         self.text_label.setAlignment(Qt.AlignCenter)
 
